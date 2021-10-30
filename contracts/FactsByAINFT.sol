865306131dc50ecf5cf20b0db776d9d542cffe3b
Hanul <hanul@hanul.me>
2021-10-30 23:32:05 +0900

    dev done

---
+++ contracts/FactsByAINFT.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.6;
+
+import "./klaytn-contracts/token/KIP17/IKIP17.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Full.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Pausable.sol";
+import "./klaytn-contracts/ownership/Ownable.sol";
+
+contract FactsByAINFT is Ownable, KIP17Full("Facts By AI", "AIFACTS"), KIP17Pausable {
+
+    IKIP17 public cbk;
+    mapping(uint256 => string) public facts;
+
+    constructor(IKIP17 _cbk) public {
+        cbk = _cbk;
+    }
+
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_exists(tokenId), "KIP17Metadata: URI query for nonexistent token");
+        
+        if (tokenId == 0) {
+            return "https://api.casesbykate.xyz/nft/0";
+        }
+
+        string memory baseURI = "https://api.casesbykate.xyz/nft/";
+        string memory idstr;
+        
+        uint256 temp = tokenId;
+        uint256 digits;
+        while (temp != 0) {
+            digits += 1;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (tokenId != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(tokenId % 10)));
+            tokenId /= 10;
+        }
+        idstr = string(buffer);
+
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, idstr)) : "";
+    }
+
+    function exists(uint256 id) public view returns (bool) {
+        return _exists(id);
+    }
+
+    function mint(uint256 id, string calldata fact) external {
+        require(cbk.ownerOf(id) == msg.sender);
+        facts[id] = fact;
+        _mint(msg.sender, id);
+    }
+}
